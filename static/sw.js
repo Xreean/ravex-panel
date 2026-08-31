ecf220e144dd5be038f2ac160527feb364ed754b
Xreean <yusufcanhasanoglu210@gmail.com>
2026-08-31 22:18:03 +0300

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "ravex-panel-v1";
+const PRECACHE = [
+  "/",
+  "/static/logo.png",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((cached) => cached || caches.match("/")))
+  );
+});
